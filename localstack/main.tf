3b53ea882096ecdc1df7294c4e12c981f74bf96c
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-03-19 01:12:51 -0300

    feat(localstack): Integrate API Gateway module

--- localstack/main.tf
+++ localstack/main.tf
@@ -46,6 +46,7 @@ provider "aws" {
     lambda         = "http://localhost:4566"
     dynamodb       = "http://localhost:4566"
     cloudwatchlogs = "http://localhost:4566"
+    apigateway     = "http://localhost:4566"
   }
 
   default_tags {
@@ -169,4 +170,16 @@ module "dynamodb" {
   source     = "../modules/dynamodb"
   table_name = "user-authentication-token"
   hash_key   = "token_id"
+}
+
+module "api_gateway" {
+  source                = "../modules/api-gateway"
+  name                  = "tech-challenge-api-local"
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.private_subnets
+  lb_dns_name           = "test-lb.localstack.external"
+  lab_role_arn          = aws_iam_role.eks_local.arn
+  auth_lambda_arn       = module.lambda_user_authentication.function_arn
+  authorizer_lambda_arn = module.lambda_user_authorizer.function_arn
+  environment           = "localstack"
 }
