454168b736987e871053372fea0b4d299a74da68
JulioCVaz <julio.oliveiravaz0@gmail.com>
2026-04-21 22:36:05 -0300

    feat: add new config to localstack ECR

--- localstack/outputs.tf
+++ localstack/outputs.tf
@@ -1,21 +1,21 @@
 output "repository_url" {
   description = "The URL of the ECR repository"
-  value       = module.ecr.repository_url
+  value       = module.ecr_api.repository_url
 }
 
 output "ecr_user_authentication_url" {
   description = "The URL of the ECR repository for user validation"
-  value       = module.ecr_user_authentication.repository_url
+  value       = module.ecr_auth.repository_url
 }
 
 output "ecr_user_authorizer_url" {
   description = "The URL of the ECR repository for user authentication"
-  value       = module.ecr_user_authorizer.repository_url
+  value       = module.ecr_authz.repository_url
 }
 
 output "ecr_notification_service_url" {
   description = "The URL of the ECR repository for notification service"
-  value       = module.ecr_notification_service.repository_url
+  value       = module.ecr_notif.repository_url
 }
 
 output "lambda_user_authentication_arn" {
@@ -32,9 +32,3 @@ output "lambda_notification_service_arn" {
   description = "The ARN of the notification service lambda"
   value       = module.lambda_notification_service.function_arn
 }
-
-output "state_bucket_arn" {
-  description = "The ARN of the S3 bucket for state and artifacts"
-  value       = module.s3.bucket_arn
-}
-
